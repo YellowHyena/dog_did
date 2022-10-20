c5f2a3ffadd7778de5f9764e720443b6442c19ce
Max Ekholm <max.m.ekholm@gmail.com>
2022-10-20 10:51:55 +0200

    moved HomePage to HomePage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Screens/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,29 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dog Did',
       theme: ThemeData(),
-      home: const MyHomePage(title: 'DogDid Home Page'),
+      home: const HomePage(title: 'DogDid Home Page'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
