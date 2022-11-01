7460f73cef9030c7781a577a64d1e6ab7cc5bfb3
Max Ekholm <max.m.ekholm@gmail.com>
2022-11-01 11:59:15 +0100

    added sign_up_page

---
+++ lib/Screens/sign_up/sign_up_page.dart
@@ -0,0 +1,40 @@
+import 'package:dog_did/screens/login/login_background.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import '../login/login_container_template.dart';
+import 'sign_up_form.dart';
+
+class SignUpPage extends StatefulWidget {
+  final VoidCallback onClickSignIn;
+
+  const SignUpPage({
+    Key? key,
+    required this.onClickSignIn,
+  }) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) => LoginContainerTemplate(
+        children: [
+          const LoginBackground(edgeInsertTop: 300),
+          const SignUpForm(),
+          RichText(
+            text: TextSpan(
+              style: const TextStyle(color: Colors.white),
+              text: 'Already have an account? ',
+              children: [
+                TextSpan(
+                  recognizer: TapGestureRecognizer()..onTap = widget.onClickSignIn,
+                  text: 'Sign in',
+                  style: TextStyle(decoration: TextDecoration.underline, color: Theme.of(context).colorScheme.primary),
+                )
+              ],
+            ),
+          ),
+        ],
+      );
+}
