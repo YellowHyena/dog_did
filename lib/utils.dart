5b8185dfc85a501092b6fa8c4df0a766666d76d0
Max Ekholm <max.m.ekholm@gmail.com>
2022-11-01 11:55:21 +0100

    added utils

---
+++ lib/utils.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Utils {
+  static final messengerKey = GlobalKey<ScaffoldMessengerState>();
+  static showSnackBar(String text) {
+    final snackBar = SnackBar(content: Text(text), backgroundColor: Colors.red);
+    messengerKey.currentState!
+      ..removeCurrentSnackBar()
+      ..showSnackBar(snackBar);
+  }
+
+  static loading(context) => showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => const Center(
+          child: CircularProgressIndicator(
+            color: Color.fromARGB(255, 255, 111, 54),
+          ),
+        ),
+      );
+}
