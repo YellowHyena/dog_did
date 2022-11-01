12e87f2c3a180870adb0d964aaea9c409236b714
Max Ekholm <max.m.ekholm@gmail.com>
2022-11-01 11:57:08 +0100

    added forgot_password_form

---
+++ lib/Screens/forgot_password/forgot_password_form.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ForgotPasswordForm extends StatefulWidget {
+  const ForgotPasswordForm({super.key});
+
+  @override
+  State<ForgotPasswordForm> createState() => _ForgotPasswordFormState();
+}
+
+class _ForgotPasswordFormState extends State<ForgotPasswordForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
