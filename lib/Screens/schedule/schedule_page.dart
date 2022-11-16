683041b180d8cd8d2cfa7e07fa2f433473443668
Max <max.m.ekholm@gmail.com>
2022-11-16 15:43:34 +0100

    fixed name and added class and widget

---
+++ lib/Screens/schedule/schedule_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SchedulePage extends StatefulWidget {
+  const SchedulePage({super.key});
+
+  @override
+  State<SchedulePage> createState() => _SchedulePageState();
+}
+
+class _SchedulePageState extends State<SchedulePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
